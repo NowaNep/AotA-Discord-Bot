b627af3215770133256c06a6f2ab05b8d1a7757d
NowaNep <40644012+NowaNep@users.noreply.github.com>
2022-05-10 01:52:12 -0500

    Started process on the index for cards first. feel free to look

---
+++ Index code but in java/src/App.java
@@ -0,0 +1,11 @@
+//used for scanner
+import java.text.*;
+//used for file and try
+import java.util.*;
+
+
+public class App {
+    public static void main(String[] args) throws Exception {
+      
+    }
+}
