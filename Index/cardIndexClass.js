b85fb1ca56e2592ee87b3543d60973497e83d95a
NowaNep <40644012+NowaNep@users.noreply.github.com>
2022-10-24 14:33:25 -0500

    roll update + progress on card index
    Added the final roll sub option (adds a number for final total rolls)

--- Index/cardIndexClass.js
+++ Index/cardIndexClass.js
@@ -1,15 +1,17 @@
 class CardFormat{
-    constructor(dice, name, flow, cardeffect){
+    constructor(dice, name, flow, cardeffect = null){
         this.dice = dice;
         this.name = name;
         this.flow = flow;
-        this.cardeffect = cardeffect;
+        if (cardeffect !=  null){
+            this.cardeffect = cardeffect;
+        }
     };
 }
 
 class Card1die extends Card{
     constructor(dice, name, flow, cardeffect,
-    die1low, die1high, die1type, proq1, proq1Effect){
+    die1low, die1high, die1type, proq1 = null, proq1Effect = null){
         super(dice, name, flow, cardeffect);
         this.die1low = die1low;
         this.die1high = die1high;
@@ -23,8 +25,8 @@ class Card1die extends Card{
 
 class Card2die extends Card1die{
     constructor(dice, name, flow, cardeffect,
-    die1low, die1high, die1type, proq1, proq1Effect,
-    die2low, die2high, die2type, proq2, proq2Effect){
+    die1low, die1high, die1type, proq1 = null, proq1Effect = null,
+    die2low, die2high, die2type, proq2 = null, proq2Effect = null){
         super(dice, name, flow, cardeffect,
         die1low, die1high, die1type, proq1, proq1Effect);
         this.die2low = die2low;
@@ -40,27 +42,109 @@ class Card2die extends Card1die{
 class Card3die extends Card2die {
     constructor(dice, name, flow, cardeffect,
     die1low, die1high, die1type, proq1, proq1Effect,
-    die2low, die2high, die2type, proq2, proq2Effect,
-    die3low, die3high, die3type, proq3, proq3Effect){
+    die2low, die2high, die2type, proq2 = null, proq2Effect = null,
+    die3low, die3high, die3type, proq3 = null, proq3Effect = null){
         super(dice, name, flow, cardeffect,
         die1low, die1high, die1type, proq1, proq1Effect,
         die2low, die2high, die2type, proq2, proq2Effect);
-        
+        this.die3low = die3low;
+        this.die3high = die3high;
+        this.die3type = die3type;
+        if (proq3 != null && proq3Effect != null){
+            this.proq3 = proq3;
+            this.proq3Effect = proq3Effect;
+        }
     }
 }
 
 class Card4die extends Card3die {
-
+    constructor(dice, name, flow, cardeffect,
+    die1low, die1high, die1type, proq1, proq1Effect,
+    die2low, die2high, die2type, proq2 = null, proq2Effect = null,
+    die3low, die3high, die3type, proq3 = null, proq3Effect = null,
+    die4low, die4high, die4type, proq4 = null, proq4Effect = null){
+        super(dice, name, flow, cardeffect,
+        die1low, die1high, die1type, proq1, proq1Effect,
+        die2low, die2high, die2type, proq2, proq2Effect,
+        die3low, die3high, die3type, proq3, proq3Effect);
+        this.die4low = die4low;
+        this.die4high = die4high;
+        this.die4type = die4type;
+        if (proq4 !== null && proq4Effect !== null){
+            this.proq4 = proq4;
+            this.proq4Effect = proq4Effect;
+        }
+    }
 }
 
 class Card5die extends Card4die {
-
+    constructor(dice, name, flow, cardeffect,
+    die1low, die1high, die1type, proq1, proq1Effect,
+    die2low, die2high, die2type, proq2 = null, proq2Effect = null,
+    die3low, die3high, die3type, proq3 = null, proq3Effect = null,
+    die4low, die4high, die4type, proq4 = null, proq4Effect = null,
+    die5low, die5high, die5type, proq5 = null, proq5Effect = null){
+        super(dice, name, flow, cardeffect,
+        die1low, die1high, die1type, proq1, proq1Effect,
+        die2low, die2high, die2type, proq2, proq2Effect,
+        die3low, die3high, die3type, proq3, proq3Effect,
+        die4low, die4high, die4type, proq4, proq4Effect);
+        this.die5low = die5low;
+        this.die5high = die5high;
+        this.die5type = die5type;
+        if (proq5 !== null && proq5Effect !== null) {
+            this.proq5 = proq5;
+            this.proq5Effect = proq5Effect;
+        }
+    }
 }
 
 class Card6die extends Card5die {
-
+    constructor(dice, name, flow, cardeffect,
+    die1low, die1high, die1type, proq1, proq1Effect,
+    die2low, die2high, die2type, proq2 = null, proq2Effect = null,
+    die3low, die3high, die3type, proq3 = null, proq3Effect = null,
+    die4low, die4high, die4type, proq4 = null, proq4Effect = null,
+    die5low, die5high, die5type, proq5 = null, proq5Effect = null,
+    die6low, die6high, die6type, proq6 = null, proq6Effect = null){
+        super(dice, name, flow, cardeffect,
+        die1low, die1high, die1type, proq1, proq1Effect,
+        die2low, die2high, die2type, proq2, proq2Effect,
+        die3low, die3high, die3type, proq3, proq3Effect,
+        die4low, die4high, die4type, proq4, proq4Effect,
+        die5low, die5high, die5type, proq5, proq5Effect);
+        this.die6low = die6low;
+        this.die6high = die6high;
+        this.die6type = die6type;
+        if (proq6 !== null && proq6Effect !== null) {
+            this.proq6 = proq6;
+            this.proq6Effect = proq6Effect;
+        }
+    }
 }
 
 class Card7die extends Card6die {
-
+    constructor(dice, name, flow, cardeffect,
+    die1low, die1high, die1type, proq1, proq1Effect,
+    die2low, die2high, die2type, proq2 = null, proq2Effect = null,
+    die3low, die3high, die3type, proq3 = null, proq3Effect = null,
+    die4low, die4high, die4type, proq4 = null, proq4Effect = null,
+    die5low, die5high, die5type, proq5 = null, proq5Effect = null,
+    die6low, die6high, die6type, proq6 = null, proq6Effect = null,
+    die7low, die7high, die7type, proq7 = null, proq7Effect = null){
+        super(dice, name, flow, cardeffect,
+        die1low, die1high, die1type, proq1, proq1Effect,
+        die2low, die2high, die2type, proq2, proq2Effect,
+        die3low, die3high, die3type, proq3, proq3Effect,
+        die4low, die4high, die4type, proq4, proq4Effect,
+        die5low, die5high, die5type, proq5, proq5Effect,
+        die6low, die6high, die6type, proq6, proq6Effect);
+        this.die7low = die7low;
+        this.die7high = die7high;
+        this.die7type = die7type;
+        if (proq7 !== null && proq7Effect !== null) {
+            this.proq7 = proq7;
+            this.die7type = die7type;
+        }
+    }
 }
--- commands/roll.js
+++ commands/roll.js
@@ -5,49 +5,62 @@ module.exports = {
 		.setName('roll')
 		.setDescription('Rolls for you, add a min and max! modifiers can be added to it as well')
     .addIntegerOption(option =>
-      option.setName('min')
-        .setDescription('The min value')
-        .setRequired(true))
+        option.setName('min')
+            .setDescription('The min value')
+            .setRequired(true))
     .addIntegerOption(option =>
-      option.setName('max')
-      .setDescription('The max value')
-      .setRequired(true))
+        option.setName('max')
+            .setDescription('The max value')
+            .setRequired(true))
     .addIntegerOption(option =>
-      option.setName('modify')
-      .setDescription('when you need to add/subtract something (AFFECTS ALL DICE)')
-      .setRequired(false))
+        option.setName('modify')
+            .setDescription('when you need to add/subtract something (AFFECTS ALL DICE)')
+            .setRequired(false))
     .addIntegerOption(option => 
-      option.setName('roll')
-      .setDescription('how many times will you roll')
-      .setRequired(false)),
+        option.setName('roll')
+            .setDescription('how many times will you roll')
+            .setRequired(false))
+    .addIntegerOption(option =>
+        option.setName('finalmodify')
+            .setDescription('adds/subtract the TOTAL of the rolled die/dice')
+            .setRequired(false)),
 	async execute(interaction) {
     //var setup
 		const min = interaction.options.getInteger('min');
-    const max = interaction.options.getInteger('max');
-    const modify = interaction.options.getInteger('modify');
-    let timesrolled = interaction.options.getInteger('roll');
-    const rolls = [];
-    const modrolls = [];
-    totalroll = 0;
-    looproll = 0;
+        const max = interaction.options.getInteger('max');
+        const modify = interaction.options.getInteger('modify');
+        let timesrolled = interaction.options.getInteger('roll');
+        let fmodify = interaction.options.getInteger('finalmodify');
+        const rolls = [];
+        const modrolls = [];
 
-    if (!timesrolled){
-      timesrolled = 1;
-    }
 
-    // the random first
-    for (i = 0; i < timesrolled; i++) {
-      looproll = Math.floor(Math.random() * (max - min) + min);
+        //checks if the fmodify is there or not
+        if (fmodify){
+            totalroll = fmodify; // gives the final roll 
+        } else {
+            totalroll = 0; // sum roll of all rolled dice if no f modify
+        }
 
-      rolls.push(looproll);
-      if (modify) {
-        looproll += modify;
-      }
-      modrolls.push(looproll);
+        looproll = 0;  // currently rolled dice in case of loop roll
 
-      totalroll += looproll;
-    }
+        if (!timesrolled){
+            timesrolled = 1;
+        }
 
-    await interaction.reply("You have rolled a " + totalroll + "!\n" + "Natural rolled dice are " + rolls);
+        // the random first
+        for (i = 0; i < timesrolled; i++) {
+            looproll = Math.floor(Math.random() * (max - min) + min);
+
+            rolls.push(looproll);
+            if (modify) {
+                looproll += modify;
+            }
+            modrolls.push(looproll);
+
+            totalroll += looproll;
+        }
+
+        await interaction.reply("You have rolled a " + totalroll + "!\n" + "Natural rolled dice are " + rolls);
 	},
 };
