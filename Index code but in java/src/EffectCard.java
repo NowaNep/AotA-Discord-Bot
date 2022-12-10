e5702b20d78c97687bbe1af34402eeae45dc7956
NowaNep <BullsDestiny180@gmail.com>
2022-12-10 16:38:45 -0600

    OK EVERYTHING IS OK

--- Index code but in java/src/EffectCard.java
+++
@@ -1,9 +0,0 @@
-public class EffectCard extends Card{
-    public EffectCard(int dice, String cardName, int flow, String cardEffects) {
-    super(dice, cardName, flow, cardEffects);
-  }
-
-  //all things are the same in card.java
-  
-
-}
